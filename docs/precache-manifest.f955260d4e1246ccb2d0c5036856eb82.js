01b4eb10aba0bdf7d2dd562082357821525cdbe8
Ivan Stanojevic <ivan.stanojevic@protonmail.com>
2020-06-20 16:11:24 +0200

    first deploy

---
+++ docs/precache-manifest.f955260d4e1246ccb2d0c5036856eb82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02791e4ca4a88387f646b80d6a0c88ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7948b463e49f333fe702",
+    "url": "/static/css/main.0567f83c.chunk.css"
+  },
+  {
+    "revision": "46935b6c79bc0a9ac234",
+    "url": "/static/js/2.f2347cac.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.f2347cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7948b463e49f333fe702",
+    "url": "/static/js/main.defc8a67.chunk.js"
+  },
+  {
+    "revision": "2db2ba9aa49a271196bd",
+    "url": "/static/js/runtime-main.b4870b2a.js"
+  }
+]);
